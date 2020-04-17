fffd4fbf60259fc3195b78a762f50ad2c48d1df6
Zhinan Guan <zhinang@amazon.com>
2020-04-17 14:12:32 -0700

    Add timer to measure event trigger message latency

--- Pinpoint-Sample/Swift/PinpointSample/PinpointSample/View/InAppMessageDemoViewController.swift
+++ Pinpoint-Sample/Swift/PinpointSample/PinpointSample/View/InAppMessageDemoViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class InAppMessageDemoViewController: UIViewController {
     var pinpoint: AWSPinpoint!
-    // var iamModule: InAppMessagingModule!
+    var timer = Timer()
+    var secondsElapsed = 0
 
-    @IBOutlet var statusLable: UILabel!
+    @IBOutlet var statusLabel: UILabel!
     @IBAction func triggerSplashMessages(_: UIButton) {
         recordEventTrigger(name: "splashMessage")
+        startTimer()
     }
 
     @IBAction func triggerDialogMessages(_: UIButton) {
         recordEventTrigger(name: "dialogMessage")
+        startTimer()
     }
 
     @IBAction func localSplashMessage(_: UIButton) {
@@ -33,7 +36,7 @@ class InAppMessageDemoViewController: UIViewController {
         super.viewDidLoad()
 
         pinpoint = (UIApplication.shared.delegate as! AppDelegate).pinpoint!
-        // iamModule = InAppMessagingModule(delegate: self)
+        pinpoint.inAppMessagingModule.delegate = self
     }
 
     private func recordEventTrigger(name: String) {
@@ -43,27 +46,59 @@ class InAppMessageDemoViewController: UIViewController {
         analyticsClient.record(event).continueOnSuccessWith { _ in
             print(">>> \(eventType) Event recorded...")
         }
-        statusLable.text = "\(eventType) event sent"
+        // statusLabel.text = "\(eventType) event sent"
+    }
+
+    func startTimer() {
+        endTimer()
+        secondsElapsed = 0
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateTime() {
+        secondsElapsed += 1
+        statusLabel.text = "Timer: \(timeFormatted(secondsElapsed))"
+    }
+
+    func endTimer() {
+        timer.invalidate()
+    }
+
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        //     let hours: Int = totalSeconds / 3600
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
 
-// extension InAppMessageDemoViewController: InAppMessagingDelegate {
-//    func primaryButtonClicked(message: AWSPinpointIAMModel) {
-//        print("\(message.name).primaryButtonClicked")
-//        // let primaryButtonURL = URL(string: message.customParam["primaryButtonURL"]!)
-//        // UIApplication.shared.openURL(primaryButtonURL!)
-//        statusLable.text = "\(message.name) primary button clicked"
-//    }
-//
-//    func secondaryButtonClicked(message: AWSPinpointIAMModel) {
-//        print("\(message.name).secondaryButtonClicked")
-//        // let secondaryButtonURL = URL(string: message.customParam["secondaryButtonURL"]!)
-//        // UIApplication.shared.openURL(secondaryButtonURL!)
-//        statusLable.text = "\(message.name) secondary button clicked"
-//    }
-//
-//    func messageDismissed(message: AWSPinpointIAMModel) {
-//        print("\(message.name).messageDismissed")
-//        statusLable.text = "\(message.name) dismissed by user"
-//    }
-// }
+extension InAppMessageDemoViewController: InAppMessagingDelegate {
+    func primaryButtonClicked(message: AWSPinpointIAMModel) {
+        print("\(message.name).primaryButtonClicked")
+        if message.name.contains("local") {
+            statusLabel.text = "\(message.name) primary button clicked"
+        } else {
+            let primaryButtonURL = URL(string: message.customParam["primaryButtonURL"]!)
+            UIApplication.shared.openURL(primaryButtonURL!)
+        }
+    }
+
+    func secondaryButtonClicked(message: AWSPinpointIAMModel) {
+        print("\(message.name).secondaryButtonClicked")
+        if message.name.contains("local") {
+            statusLabel.text = "\(message.name) secondary button clicked"
+        } else {
+            let secondaryButtonURL = URL(string: message.customParam["secondaryButtonURL"]!)
+            UIApplication.shared.openURL(secondaryButtonURL!)
+        }
+    }
+
+    func messageDismissed(message: AWSPinpointIAMModel) {
+        print("\(message.name).messageDismissed")
+    }
+
+    func displayInvoked(message: AWSPinpointIAMModel) {
+        endTimer()
+        statusLabel.text = "\(message.name) received time: \(timeFormatted(secondsElapsed))"
+    }
+}
